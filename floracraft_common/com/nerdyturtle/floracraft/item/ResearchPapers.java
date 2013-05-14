f7a5ec9af1e98e6a6f08660017881a733a262e9f
nerdyturtle <nerdyturtle.mc@gmail.com>
2013-05-14 19:12:29 -0400

    Added base research paper

---
+++ floracraft_common/com/nerdyturtle/floracraft/item/ResearchPapers.java
@@ -0,0 +1,20 @@
+package com.nerdyturtle.floracraft.item;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.item.Item;
+
+public class ResearchPapers extends Item{
+
+    public ResearchPapers(int id) {
+        super(id);
+        this.setUnlocalizedName("researchPapers");
+    }
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister iconRegister) {
+
+        itemIcon = iconRegister.registerIcon("floracraft" + ":" + this.getUnlocalizedName().substring(5));
+    }
+
+}
